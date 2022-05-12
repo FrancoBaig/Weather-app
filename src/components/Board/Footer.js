5d2efa0946be0e94170307c35aaa16cea4c7b1e7
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-05-12 17:03:37 -0300

    board v0

--- src/components/Board/Footer.js
+++ src/components/Board/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-function Footer() {}
+function Footer() {
+    return (
+        <footer className="footer">
+            created by <a href="#">FrancoBaig</a> - devChallenges.io
+        </footer>
+    );
+}
 
 export default Footer;
